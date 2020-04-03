d3f6eae76dabdda135212a3905453d4b87ff9ebd
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-03 18:03:20 +0200

    prima parte ok

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 $('document').ready(function(){
+
+    var htmlGiorno = $('#calendar-template').html();
+    var templateGiorno = Handlebars.compile(htmlGiorno);
+
+
     var dataIniziale = moment('2018-01-01');
     console.log(dataIniziale);
 
+    var giorniMese = dataIniziale.daysInMonth();
+    console.log(giorniMese);
 
+    var nomeMese = dataIniziale.format('MMMM');
+    console.log(nomeMese);
 
-
+    for (var i = 1; i <= giorniMese; i++) {
+        var giornoDaInserire = {
+            day: i + ' ' + nomeMese
+        }
+        var templateFinale = templateGiorno(giornoDaInserire);
+        $('#calendar').append(templateFinale);
+    };
 
 
 });
